b5f9d1673289ac8ea88e5a507038da4aacca0a51
diivane003-lgtm <diivane003@gmail.com>
2026-08-04 11:36:21 -0500

    primera version

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const stars = document.getElementById("stars");
+
+for(let i = 0; i < 250; i++){
+
+    const star = document.createElement("div");
+
+    star.className = "star";
+
+    star.style.left = Math.random()*100 + "%";
+
+    star.style.top = Math.random()*100 + "%";
+
+    star.style.animationDelay = Math.random()*3 + "s";
+
+    stars.appendChild(star);
+
+}
+
+document.getElementById("entrar").addEventListener("click",()=>{
+
+    alert("Bienvenido a Nuestro Universo 🩷🌌");
+
+});
