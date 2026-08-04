d1f26de2c77576a670b42baf290f9f91dbfd1c71
diivane003-lgtm <diivane003@gmail.com>
2026-08-04 13:23:39 -0500

    segunda version

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-function abrirSobre(){
+.corazon{
 
-document.getElementById("inicio").style.display="none";
+    position:absolute;
 
-document.getElementById("sobreContainer").style.display="block";
+    color:#ff7eb9;
+
+    font-size:25px;
+
+    animation:flotar 5s linear forwards;
+
+    pointer-events:none;
 
 }
 
 
+@keyframes flotar{
 
-function mostrarCarta(){
+    0%{
 
-document.querySelector(".sobre")
-.classList.add("abierto");
+        transform:translateY(0) scale(1);
+
+        opacity:1;
+
+    }
+
+
+    100%{
+
+        transform:translateY(-700px) scale(1.8);
+
+        opacity:0;
+
+    }
 
 }
